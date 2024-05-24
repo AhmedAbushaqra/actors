f3c0a386286c2cba82a112afdb377350fefce33a
AhmedAbushaqra <ahmedhossin26@yahoo.com>
2024-05-24 16:00:02 -0700

    save image in local storage

--- lib/screens/populars/image_display.dart
+++ lib/screens/populars/image_display.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 class ImageDisplay extends StatefulWidget {
   final String image;
@@ -9,7 +13,9 @@ class ImageDisplay extends StatefulWidget {
 }
 
 class _ImageDisplayState extends State<ImageDisplay> {
-  bool saved =false;
+  bool saved = false;
+  bool saving = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,17 +27,21 @@ class _ImageDisplayState extends State<ImageDisplay> {
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               InkWell(
-                onTap: (){
-                  if(!saved){}
+                onTap: () {
+                  if (!saved) {
+                    fetchAndSaveImage(widget.image);
+                  }
                 },
                 child: Padding(
                   padding: const EdgeInsets.all(20),
                   child: SizedBox(
-                    child: Row(
+                    child: saving?const CircularProgressIndicator():Row(
                       children: [
-                        Text(saved?"Saved":"Save",style: const TextStyle(fontWeight: FontWeight.w900),),
+                        Text(saved ? "Saved" : "Save",
+                          style: const TextStyle(fontWeight: FontWeight.w900),),
                         const SizedBox(width: 10,),
-                        Icon(saved?Icons.download_done:Icons.save,size: 28,),
+                        Icon(
+                          saved ? Icons.download_done : Icons.save, size: 28,),
                       ],
                     ),
                   ),
@@ -48,5 +58,34 @@ class _ImageDisplayState extends State<ImageDisplay> {
         ],
       ),
     );
+  }
+
+  Future<void> fetchAndSaveImage(String url) async {
+    setState(() {
+      saving = true;
+    });
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final directory = await getApplicationDocumentsDirectory();
+        final path = '${directory.path}/downloaded_image.jpg';
+
+        final file = File(path);
+        await file.writeAsBytes(response.bodyBytes);
+
+        print('Image saved to $path');
+        setState(() {
+          saved = true;
+        });
+      } else {
+        throw Exception('Failed to load image');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+    setState(() {
+      saving = false;
+    });
   }
 }
